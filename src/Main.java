93b70f9c6fbb2fccf1fca993cc1652f4ad0a895b
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-11-12 10:43:56 +0900

    main default

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import problem.Programmers258712;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Programmers258712 programmers258712 = new Programmers258712();
+
     }
 }
