2a150d86218289e21844ea22c30229f5f892ee1c
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-11 15:52:32 +0900

    [실패] https://www.acmicpc.net/problem/1697

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,34 @@
 import java.io.*;
+import java.sql.Array;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[][] map;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int N, K;
+    static int[] position = {1, -1, 2};
+    static int time = 0;
+    static int[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
+        visited = new int[100001];
 
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = line.charAt(j) - '0';
-            }
-        }
-
-        bfs(0, 0);
-
-        System.out.println(map[N - 1][M - 1]);
+        bfs();
     }
 
-    // x,y는 첫 시작 좌표를 나타낸다.
-    public static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-
-        // 첫 방문
-        queue.add(new int[]{x, y});
+    // time
+    // arrayList -> array
+    // 범위 검사
+    public static void bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(N);
 
         while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int cx = current[0];
-            int cy = current[1];
 
-            for (int i = 0; i < 4; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-
-                if (map[nx][ny] == 1) {
-                    queue.add(new int[]{nx, ny});
-
-                    map[nx][ny] = map[cx][cy] + 1;
-                }
-            }
         }
     }
 }
