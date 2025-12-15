ca6ff6dd67ac5cb9d9cdf5d0850812d4e217b8cd
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-15 10:35:46 +0900

    https://www.acmicpc.net/problem/7562

--- src/Main.java
+++ src/Main.java
@@ -2,49 +2,76 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, K;
-    static int[] visited = new int[100001];
+    static int[][] move = {{-1, 2}, {-2, 1}, { -1, -2}, {-2, -1},
+            {1, 2}, {2, 1}, {1, -2}, {2, -1}};
+    static int[][] map;
+    static List<Integer> resultList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-        if (N == K) {
-            System.out.println(0);
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int size = Integer.parseInt(st.nextToken());
+
+            map = new int[size][size];
+
+            st = new StringTokenizer(br.readLine());
+            int X = Integer.parseInt(st.nextToken());
+            int Y = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            int aX = Integer.parseInt(st.nextToken());
+            int aY = Integer.parseInt(st.nextToken());
+
+            bfs(X, Y, aX, aY, size);
+        }
+
+        for (int num : resultList) {
+            System.out.println(num);
+        }
+    }
+
+    public static void bfs(int X, int Y, int aX, int aY, int size) {
+        if(X == aX && Y == aY) {
+            resultList.add(0);
             return;
         }
 
-        bfs();
-    }
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{X, Y});
 
-    public static void bfs() {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(N);
-        visited[N] = 1; // 시작점 방문 처리?
+        // 첫 방문 체크
+        map[X][Y] = 1;
 
         while (!queue.isEmpty()) {
-            int current = queue.poll();
+            int[] current = queue.poll();
+            int cX = current[0];
+            int cY = current[1];
 
-            int[] nextPositions = {current - 1, current + 1, current * 2};
+            // 나이트가 이돌할 수 있는 좌표 모두 순회
+            for (int i = 0; i < 8; i++) {
+                int[] d = move[i];
 
-            for (int next : nextPositions) {
-                // 목표 지점에 도달했는지 확인
-                if (next == K) {
-                    System.out.println(visited[current]);
-                    return;
-                }
+                int dX = cX + d[0];
+                int dY = cY + d[1];
 
-                // 유효성 검사 ( 범위를 벗어나지 않고 & 방문하지 않았던 곳만
-                if (next >= 0 && next < 100001 && visited[next] == 0) {
-                    visited[next] = visited[current] + 1; // 시간 1초 증가
-                    queue.add(next);
+                // 범위 유효성 검사
+                if (dX < 0 || dY < 0 || dX >= size || dY >= size) continue;
+
+                if (map[dX][dY] == 0) {
+                    map[dX][dY] = map[cX][cY] + 1;
+                    queue.add(new int[]{dX, dY});
+
+                    if (aX == dX && aY == dY) {
+                        resultList.add(map[aX][aY] - 1);
+                    }
                 }
             }
         }
     }
 }
-// visitied : 내가 이곳을 방문했는지 체크, 시작점으로부터 여기까지 오는데 걸린 시간(거리)
 
