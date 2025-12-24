2dbb2c13e56e4211c554d873c4cc316a00c058ef
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-12-24 13:06:31 +0900

    [실패] https://www.acmicpc.net/problem/16928

--- src/Main.java
+++ src/Main.java
@@ -2,86 +2,66 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[][] map;
-    static Queue<int[]> queue = new LinkedList<>();
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static boolean isCompleted = true;
-    static int result = 0;
+    static int N,M;
+    static int[] visited;
+    static int[] move;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        M = Integer.parseInt(st.nextToken()); // 열
-        N = Integer.parseInt(st.nextToken()); // 행
+        N = Integer.parseInt(st.nextToken()); // 사다리의 수
+        M = Integer.parseInt(st.nextToken()); // 뱀의 수
 
-        map = new int[N][M];
+        visited = new int[101];
+        move = new int[101];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N+M; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
 
-                if (map[i][j] == 1) {
-                    queue.add(new int[]{i, j});
-                }
+            int x = Integer.parseInt(st.nextToken());
 
-                if (map[i][j] == 0) {
-                    isCompleted = false;
-                }
-            }
-        }
-
-        if (isCompleted) {
-            System.out.println(0);
-            return;
+            move[x] = Integer.parseInt(st.nextToken());
         }
 
         bfs();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 0) {
-                    System.out.println(-1);
-                    return;
-                }
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] > result) {
-                    result = map[i][j];
-                }
-            }
-        }
-
-        System.out.println(result - 1);
     }
 
     public static void bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+
         while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
+            int target = queue.poll();
 
-            for (int i = 0; i < 4; i++) {
-                int nx = curr[0] + dx[i];
-                int ny = curr[1] + dy[i];
+            for (int i = 1; i <= 6; i++) {
+                int next = target + i;
 
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                // next가 100이 되면 count를 반환해야한다.
 
-                if (map[nx][ny] == 0) {
-                    queue.add(new int[]{nx, ny});
-                    map[nx][ny] = map[curr[0]][curr[1]] + 1;
+                if(next > 100) continue;
+
+                // 이미 방문 했는지 체크하기
+                if (visited[next] == 0) {
+                    if (move[next] != 0) {
+                        visited[next] = 1; // 방문 체크
+                        queue.add(move[next]);
+                    }
                 }
             }
         }
     }
 }
 
-// M : 상자 가로 칸의 수, N : 상자 세로 칸의 수
-// 익은 : 1, 익지 않은 : 0, 없는 : -1
-// 출발 지점이 여러개 : map 초기화 할 때 미리 저장해야 한다.
+// 100번 칸에 도착하기 위해 주사위를 굴려야 하는 횟수의 최솟값? -> bfs
+// 게임의 크기 10 * 10 -> 1 ~ 100 까지 순서대로 적혀있음
+// 모든 칸은 최대 하나 사다리 또는 뱀 있음
+// 동시에 두 가지를 모두 가지고 있는 경우는 없다 -> 방문 했던 곳은 또 다시 방문하지 않는다.
 
-// 출력
+// 방문 체크
+// map
+
+// 주사위 돌린 횟수를 어떻게 카운트 하면될까?
+
+// --------
+// visited -> 방문했다만 저장하는 대신 여기까지 오는데 걸린 횟수 저장
